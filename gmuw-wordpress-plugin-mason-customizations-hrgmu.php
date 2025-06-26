b8a3f65f60c5cc62fe9f96a326d9fbe84d313819
Jan Macario <jmacario@gmu.edu>
2025-06-26 11:24:20 -0400

    Modifies the function which restricts URLs to require a login and redirects to the login page to allow for multiple patterns of restricted URLs.

--- gmuw-wordpress-plugin-mason-customizations-hrgmu.php
+++ gmuw-wordpress-plugin-mason-customizations-hrgmu.php
@@ -28,29 +28,41 @@ __FILE__,
 
 
 //redirect requests for certain URLs to login
-add_action( 'wp', 'gmuw_customizations_hrgmu_restrict_access_to_fses_area', 3 );
-function gmuw_customizations_hrgmu_restrict_access_to_fses_area(){
-
-	//set regex for URLs for the faculty/staff experience survey to require a login
-	//only matches URLs under the /faculty-staff-experience-survey/ path, not that page itself, which serves as a landing page
-	$fses_url_regex='/^faculty-staff-experience-survey\/.+/i';
+add_action( 'wp', 'gmuw_customizations_hrgmu_restrict_access_to_urls_by_pattern', 3 );
+function gmuw_customizations_hrgmu_restrict_access_to_urls_by_pattern(){
 
 	//set login redirect url - will require a login which will redirect to the requested page
 	$login_redirect_url=home_url() . '/wp-login.php?redirect_to=' . get_permalink();
+
+	//create array of regex patterns to define URLs that should require a login
+	$restricted_url_patterns=array();
+
+	//add patterns to array
+
+	//faculty/staff experience survey page
+	//only matches URLs under the /faculty-staff-experience-survey/ path, not that page itself, which serves as a landing page
+	$restricted_url_patterns[]='/^faculty-staff-experience-survey\/.+/i';
+	//performance evaluation resources page
+	$restricted_url_patterns[]='/^performance-evaluations\/performance-evaluation-resources/i';
 
 	// Get current page URL slug
 	global $wp;
 	$current_slug = add_query_arg( array(), $wp->request );
 
-	//is the requested URL one which matches our restriction criteria?
-	if ( preg_match($fses_url_regex, $current_slug) ) {
+	//loop through restriction patterns
+	foreach ($restricted_url_patterns as $restricted_url_pattern) {
 
-	// is the user NOT logged in?
-		if ( !is_user_logged_in() ) {
-		
-			//perform redirect to login page
-			wp_redirect($login_redirect_url);
-			exit;
+		//is the requested URL one which matches this restriction pattern?
+		if ( preg_match($restricted_url_pattern, $current_slug) ) {
+
+		// is the user NOT logged in?
+			if ( !is_user_logged_in() ) {
+
+				//perform redirect to login page
+				wp_redirect($login_redirect_url);
+				exit;
+
+			}
 
 		}
 
